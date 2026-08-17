079863c8636b895ac0ecb91e302d943ec2aa1d6d
Takyon <bengilder99@gmail.com>
2026-08-17 18:40:01 +0100

    Added polynomials files and made init function

--- polynomial.c
+++ polynomial.c
@@ -1,12 +1,18 @@
 #include "polynomial.h"
+#include "rational.h"
+#include <stdlib.h>
+#include <string.h>
 
 struct polynomial_internal {
   unsigned int degree;
-  struct rational *coefficients;
+  rational *coefficients;
 };
 
-polynomial *init_polynomial(unsigned int degree,
-                            struct rational *coefficients) {
-  polynomial *_temp;
+polynomial init_polynomial(unsigned int degree, rational *coefficients) {
+  polynomial _temp;
+  _temp->degree = degree;
+  _temp->coefficients = calloc((size_t)degree, sizeof(rational));
+  memcpy(_temp->coefficients, coefficients,
+         (sizeof(rational) * (size_t)degree));
   return _temp;
 }
--- polynomial.h
+++ polynomial.h
@@ -1,6 +1,9 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
+#include "rational.h"
 
 typedef struct polynomial_internal *polynomial;
+
+polynomial init_polynomial(unsigned int, rational *);
 
 #endif // !POLYNOMIAL_H
